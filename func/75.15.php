06a5fa95ee86f9ebddd3a453f4de90fa7b11c8c0
Pavel Ryzhenkov <asdsa@gg.ss>
2022-11-19 12:27:31 +0000

    75.15.php

--- func/75.15.php
+++ func/75.15.php
@@ -3,7 +3,7 @@
     $string = 'prOYs44 ';
 
     if(strlen($string > 5)) {
-        $str = substr($string, 0, 6);
+        $str = substr($string, 0, 6) .'...';
     }
     else {
         $str = $string;
