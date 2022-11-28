ab62cb977a41c79d672273087e6eb56e9466c489
Pavel Ryzhenkov <asdsa@gg.ss>
2022-11-28 14:35:15 +0000

    .

--- mnogomernost/69.2.php
+++ mnogomernost/69.2.php
@@ -1,5 +1,12 @@
 <?php
 	$arr = [[1, 2], [3, 4], [5, 6]];
-   
-    echo array_sum(array_column($arr, 'number'));
+    $sum = 0;
+    foreach ($arr as $sub) {
+        foreach($sub as $elem) {
+            $sum += $elem;
+            
+        }
+        
+    }
+    echo $sum;
 ?>
